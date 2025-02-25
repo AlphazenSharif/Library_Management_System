b7ee981c668cf36fc83fead73cef4992a6433119
AlphazenSharif <sharif@alphazentechnologies.com>
2025-02-25 09:11:32 +0000

    changes to library_membership.py

---
+++ library_management/library/doctype/library_membership/library_membership.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, sarif and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Library Membership", {
+// 	refresh(frm) {
+
+// 	},
+// });
